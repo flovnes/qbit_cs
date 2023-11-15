5292f8b8bb1cd6c0713fa467f94af8ae02242dd3
flovnes <whyflovnes@gmail.com>
2023-11-15 10:48:14 +0200

    first

---
+++ Linear_programs/qbit1/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("{0}", Math.Round((0.3*Math.Pow(5,2)-15.0)/(3.5+Math.Pow(2,2))));
+    }
+}
---
+++ Loops/startriangle/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Solution();
+    }
+    static void Solution()
+    {
+        byte num = byte.Parse(Console.ReadLine());
+
+        for (int line = 1; line <= num * 2 - 1; line++)
+        {
+            if (line <= num)
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    bool firstStar = true;
+                    if (i > num - line) {
+                        if (i == num)
+                        {
+                            Console.Write("*");
+                        }
+                        else if (i == num - line + 1)
+                        {
+                            if (firstStar == true) {
+                                if (line == 1) {
+                                    Console.Write("*".PadLeft(num*2));
+                                } else {
+                                Console.Write("* ".PadLeft(num*2));
+                                firstStar = false;
+                            }
+                        }
+                    }
+                    else
+                    { Console.Write("+ ");}
+
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    bool firstStar = true;
+                    if (i > line - num)
+                    {
+                        if (i == num)
+                        {
+                            Console.Write("*");
+                        }
+                        else if (i == line - num + 1)
+                        {
+                            if (firstStar == true) {
+                                if (line == num*2-1) {
+                                    Console.Write("*".PadLeft(num*2));
+                                } else {
+                                Console.Write("* ".PadLeft(num*2));
+                                firstStar = false;
+                            }
+                        }
+                        else
+                        {
+                            Console.Write("+ ");
+                        }
+                        }
+                    }
+                }
+            Console.WriteLine();
+            }
+        }
+    }
+}
